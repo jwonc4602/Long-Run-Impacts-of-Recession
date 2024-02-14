ee79615df9d894c00b6c4c4bf80059c211a3f838
Heyucheng Zhang <heyucheng.zhang@mail.utoronto.ca>
2024-02-14 14:20:59 -0500

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -15,6 +15,7 @@ library(readxl)
 # Read data
 unemployment <- read_excel("inputs/data/BLS_all_archived_counties7689aa (UNOFFICIAL).xlsx")
 personal_income <- read_csv("inputs/data/CA1_1969_2013_ALL.csv")
+bussiness <- read_csv("inputs/data/CA25_1969_2000_ALL.csv")
 
 # Group unemployment data by state and year, calculating average for labor force, employed, unemployed, and unemployment rate
 cleaned_unemployment <-
@@ -33,19 +34,51 @@ cleaned_four_states <-
   cleaned_unemployment |>
   filter(state_name %in% c("New York", "Florida", "Texas", "California"))
 
-# Get records for the United States
-USA_personal_income <-
-  personal_income |>
-  filter(GeoName == "United States")
-
-# Filter for the description of "Per capita personal income (dollars) 2/" and select the Description column and the year columns from 1969 to 2013
 cleaned_personal_income <-
-  USA_personal_income |>
+  personal_income |>
+  # Get records for the United States
+  filter(GeoName == "United States") |>
+  # Filter for the description of "Per capita personal income (dollars) 2/"
   filter(Description == "Per capita personal income (dollars) 2/") |>
-  select(Description, `1969`:`2013`)
+  # Select the Description column and the year columns from 1976 to 2013
+  select(Description, `1976`:`2013`) |>
+  # Swap rows and columns
+  gather(key = "year", value = "value", -Description) |>
+  spread(key = "Description", value = "value")
+
+cleaned_bussiness_pattern1 <-
+  bussiness |>
+  # Get records for the United States
+  filter(GeoName == "United States") |>
+  # Filter for the description of "Total full-time and part-time employment", "Wage and salary employment", "Proprietors employment"
+  filter(Description %in% c("Total full-time and part-time employment", "Wage and salary employment", "Proprietors employment")) |>
+  # select the Description column and the year columns from 1969 to 2000
+  select(Description, `1969`:`2000`) |>
+  # Swap rows and columns
+  gather(key = "year", value = "value", -Description) |>
+  spread(key = "Description", value = "value") |>
+  # Order columns
+  select("year", "Total full-time and part-time employment", "Wage and salary employment", "Proprietors employment")
+
+cleaned_bussiness_pattern2 <-
+  bussiness |>
+  # Get records for the United States
+  filter(GeoName == "United States") |>
+  # Filter for the description of "Private nonfarm employment", "Agricultural services, forestry, and fishing", "Mining", "Construction", "Manufacturing", "Transportation and public utilities", "Wholesale trade", "Retail trade", "Finance, insurance, and real estate", "Services"
+  filter(Description %in% c("Private nonfarm employment", "Agricultural services, forestry, and fishing", "Mining", "Construction", "Manufacturing", "Transportation and public utilities", "Wholesale trade", "Retail trade", "Finance, insurance, and real estate", "Services")) |>
+  # select the Description column and the year columns from 1969 to 2000
+  select(Description, `1969`:`2000`) |>
+  # Swap rows and columns
+  gather(key = "year", value = "value", -Description) |>
+  spread(key = "Description", value = "value") |>
+  # Order columns
+  select("year", "Private nonfarm employment", "Agricultural services, forestry, and fishing", "Mining", "Construction", "Manufacturing", "Transportation and public utilities", "Wholesale trade", "Retail trade", "Finance, insurance, and real estate", "Services")
 
 #### Save data ####
 write_csv(cleaned_unemployment, "outputs/data/cleaned_unemployment.csv")
 write_csv(cleaned_four_states, "outputs/data/cleaned_four_states.csv")
 write_csv(cleaned_personal_income, "outputs/data/cleaned_personal_income.csv")
+write_csv(cleaned_bussiness_pattern1, "outputs/data/cleaned_bussiness_pattern1.csv")
+write_csv(cleaned_bussiness_pattern2, "outputs/data/cleaned_bussiness_pattern2.csv")
+
 
